0de8bcef8c8beda400ed2e3842333c4521bc1cd1
crogers3 <crogers3@mit.edu>
2016-01-18 10:57:54 -0500

    Removed individual constraint files

--- drake/solvers/NPCPP/ConstantConstraint.cpp
+++
@@ -1,8 +0,0 @@
-#include "ConstantConstraint.h"
-
-namespace drake {
-ConstantConstraint::ConstantConstraint(double desired_value,
-    int xdim,
-    int jAvar) : BoundingBoxConstraint(
-        desired_value, desired_value, xdim, jAvar) {}
-} // namespace drake
